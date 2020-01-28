248c2e8066736bd7b8489becb9e1edb76a283c87
aipstudio <aipstudio@mail.ru>
2020-01-28 17:08:46 +0300

    1

--- web/main.go
+++ web/main.go
@@ -2,15 +2,12 @@ package main
 
 import (
 	"fmt"
-	"github.com/OsoianMarcel/claymore-go"
 	"log"
 	"net/http"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "TroloLO!\n")
-	cc := claymore.NewClient("109.172.77.189:13333")
-	fmt.Fprintln(cc.GetStats())
 }
 
 func main() {
