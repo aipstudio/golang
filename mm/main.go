30d90dd16e231e1a17cf9d4d2330c8e208f306ba
aipstudio <aipstudio@mail.ru>
2020-02-07 13:19:08 +0300

    1

--- mm/main.go
+++ mm/main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/OsoianMarcel/claymore-go"
 	"log"
 	"net/http"
+	"strconv"
 	"time"
 )
 
@@ -71,18 +72,20 @@ type StatsResponse struct {
 	Extra ExtraResponse       `json:"extra"`
 }
 
-var x1, x2, x3 []int
-var x1sum, x2max, x2min, x3max, x3min, x2rmax, x2rmin, x3rmax, x3rmin int
+var Mhs, T, C []int
+var Mhssum, Tmax, Tmin, Cmax, Cmin, Trmax, Trmin, Crmax, Crmin int
+var count_message int = 0
 
 func get_json_miner() {
-	x2max = 80
-	x2min = 0
-	x3max = 100
-	x3min = 0
-	x1sum = 0
-	x1 = nil
-	x2 = nil
-	x3 = nil
+	Tmax = 80
+	Tmin = 0
+	Cmax = 100
+	Cmin = 0
+	Mhssum = 0
+	Mhs = nil
+	T = nil
+	C = nil
+
 	mm := [3]string{"109.172.77.189:33332", "109.172.77.189:33333", "109.172.77.189:33337"}
 	//mm := []string{"109.172.77.189:33332"}
 	for i := 0; i < len(mm); i++ {
@@ -105,39 +108,79 @@ func get_json_miner() {
 		//fmt.Println(app.Data.Stats.EthReport.MhsPerGpu)
 		for _, row := range app.Data.Stats.EthReport.MhsPerGpu {
 			//fmt.Println(row.Mhs)
-			x1 = append(x1, row.Mhs)
-			x1sum = x1sum + row.Mhs
+			Mhs = append(Mhs, row.Mhs)
+			Mhssum = Mhssum + row.Mhs
 		}
 		for _, row := range app.Data.Stats.TempAndFanReports {
-			x2 = append(x2, row.Temp)
-			if row.Temp > x2min {
-				x2rmax = row.Temp
-				x2min = row.Temp
+			T = append(T, row.Temp)
+			if row.Temp > Tmin {
+				Trmax = row.Temp
+				Tmin = row.Temp
 			}
-			if row.Temp < x2max {
-				x2rmin = row.Temp
-				x2max = row.Temp
+			if row.Temp < Tmax {
+				Trmin = row.Temp
+				Tmax = row.Temp
 			}
-			x3 = append(x3, row.Fan)
-			if row.Fan > x3min {
-				x3rmax = row.Fan
-				x3min = row.Fan
+			C = append(C, row.Fan)
+			if row.Fan > Cmin {
+				Crmax = row.Fan
+				Cmin = row.Fan
 			}
-			if row.Fan < x3max {
-				x3rmin = row.Fan
-				x3max = row.Fan
+			if row.Fan < Cmax {
+				Crmin = row.Fan
+				Cmax = row.Fan
 			}
 		}
 	}
+	if Mhssum < 380000 || Trmax > 80 || Trmin < 40 || Crmax > 80 || Crmin < 40 {
+		count_message = count_message + 1
+		fmt.Println(count_message)
+		if count_message > 9 {
+			count_message = 0
+			MakeRequest()
+		}
+	}
+
+}
+
+func MakeRequest() {
+	var y string = time.Now().Format("15:04:05 01-02-2006") + " Mhs=" + strconv.Itoa(Mhssum) + " Tmax=" + strconv.Itoa(Trmax) + " Tmin=" + strconv.Itoa(Trmin) + " Cmax=" + strconv.Itoa(Crmax) + " Cmin=" + strconv.Itoa(Crmin)
+	var yy string = ``
+	for i := 0; i < len(Mhs); i++ {
+		yy = yy + strconv.Itoa(Mhs[i]) + ` - ` + strconv.Itoa(T[i]) + ` - ` + strconv.Itoa(C[i]) + `\n`
+	}
+	data := []byte(`{
+		"embeds": [
+		  {
+			"title": "MM",
+			"description": "` + y + `",
+			"color": 564300,
+			"fields": [{"name": "Report","value": "` + yy + `"}]
+		}
+		]
+	  }`)
+	r := bytes.NewReader(data)
+	resp, err := http.Post("https://discordapp.com/api/webhooks/642252309182808074/Rnx0v-0hKSuX-GYmNrpegEVjUsXm0I6K703L2G85lsp2kM-TmYqN3-zYcR4IuFAv_blh", "application/json", r)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	var result map[string]interface{}
+	json.NewDecoder(resp.Body).Decode(&result)
+	//log.Println(result)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, time.Now().Format("15:04:05 01-02-2006"), "\nMhs=", x1sum, "\nTmax=", x2rmax, "\nTmin=", x2rmin, "\nCmax=", x3rmax, "\nCmin=", x3rmin, "\n")
-
-	for i := 0; i < len(x1); i++ {
-		fmt.Fprintln(w, x1[i], x2[i], x3[i])
+	fmt.Fprintln(w, time.Now().Format("15:04:05 01-02-2006"), "\nMhs=", Mhssum, "\nTmax=", Trmax, "\nTmin=", Trmin, "\nCmax=", Crmax, "\nCmin=", Crmin, "\n")
+	for i := 0; i < len(Mhs); i++ {
+		fmt.Fprintln(w, Mhs[i], T[i], C[i])
 	}
-	//for _, v := range x1 {
+
+	//for _, v := range Mhs {
 	//	fmt.Fprintln(w, v)
 	//}
 }
@@ -151,5 +194,5 @@ func get_json_miner_timer() {
 func main() {
 	go get_json_miner_timer()
 	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":3333", nil))
+	log.Fatal(http.ListenAndServe(":8009", nil))
 }
